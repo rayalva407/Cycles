7d303db337adf873936449182125cbe3c68e2d67
rayalva407 <rayalva407@gmail.com>
2020-10-28 14:22:10 -0600

    adds tracker name show function

--- cycles-backend/app/controllers/trackers_controller.rb
+++ cycles-backend/app/controllers/trackers_controller.rb
@@ -3,4 +3,23 @@ class TrackersController < ApplicationController
     trackers = Tracker.all
     render json: trackers
   end
+
+  def new
+    tracker = Tracker.new
+  end
+
+  def create
+    tracker = Tracker.new(tracker_params)
+    if tracker.save
+      render json: tracker, except: [:created_at, :updated_at]
+    else
+      render json: {message: "Only letters"}
+    end
+  end
+
+  private
+
+    def tracker_params
+      params.require(:tracker).permit(:name)
+    end
 end
--- cycles-frontend/src/index.js
+++ cycles-frontend/src/index.js
@@ -1,5 +1,32 @@
 const BACKEND_URL = 'http://localhost:3000';
+const TRACKERS_URL = `${BACKEND_URL}/trackers`
+const trackerForm = document.querySelector("#create-tracker-form")
+const formData = document.querySelector("#new-tracker-name")
+const welcomeDiv = document.querySelector("#welcome-div")
 
-fetch(`${BACKEND_URL}/trackers`)
-  .then(response => response.json())
-  .then(parsedResponse => console.log(parsedResponse))
+function welcomeShow(object) {
+  const welcomeMessage = document.createElement('h2')
+  welcomeMessage.innerText = `Welcome ${object.name}!`
+  trackerForm.remove()
+  welcomeDiv.appendChild(welcomeMessage)
+
+}
+
+trackerForm.addEventListener('submit', function(e) {
+  e.preventDefault
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: formData.value
+    })
+  };
+
+  fetch(TRACKERS_URL, configObj)
+    .then(response => response.json())
+    .then(object => welcomeShow(object))
+    .catch(error => alert(error.message))
+})
