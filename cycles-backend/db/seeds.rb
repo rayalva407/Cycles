2b7e6fd0f27f73c5fa6d3af92bd4caae15c93db8
rayalva407 <rayalva407@gmail.com>
2020-10-28 08:31:40 -0600

    adds Tracker resource

--- cycles-backend/db/seeds.rb
+++ cycles-backend/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Tracker.create(name: "Ana")
+Tracker.create(name: "Jessie")
+Tracker.create(name: "Beth")
