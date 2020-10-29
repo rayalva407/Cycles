9505912774e82a1993b9e13529b638c2b0f537a6
rayalva407 <rayalva407@gmail.com>
2020-10-29 12:51:50 -0600

    adds cycle event listener

--- cycles-frontend/src/index.js
+++ cycles-frontend/src/index.js
@@ -1,19 +1,25 @@
 const BACKEND_URL = 'http://localhost:3000';
 const TRACKERS_URL = `${BACKEND_URL}/trackers`
 const trackerForm = document.querySelector("#create-tracker-form")
-const formData = document.querySelector("#new-tracker-name")
+const cycleForm = document.createElement('form')
+cycleForm.setAttribute("id", "cycle-startdate-form")
+cycleForm.innerHTML = "<label for='new-cycle'>Enter your last period's start date:</label><input type='date' id='new-cycle' name='new-cycle'><input type='submit' value='Submit'>"
+const trackerData = document.querySelector("#new-tracker-name")
+const cycleData = document.querySelector("#new-cycle")
 const welcomeDiv = document.querySelector("#welcome-div")
+
 
 function welcomeShow(object) {
   const welcomeMessage = document.createElement('h2')
   welcomeMessage.innerText = `Welcome ${object.name}!`
   trackerForm.remove()
   welcomeDiv.appendChild(welcomeMessage)
-
+  welcomeDiv.appendChild(cycleForm)
 }
 
 trackerForm.addEventListener('submit', function(e) {
-  e.preventDefault
+  console.log(e)
+  e.preventDefault()
   let configObj = {
     method: "POST",
     headers: {
@@ -21,7 +27,7 @@ trackerForm.addEventListener('submit', function(e) {
       "Accept": "application/json"
     },
     body: JSON.stringify({
-      name: formData.value
+      name: trackerData.value
     })
   };
 
@@ -29,4 +35,20 @@ trackerForm.addEventListener('submit', function(e) {
     .then(response => response.json())
     .then(object => welcomeShow(object))
     .catch(error => alert(error.message))
+})
+
+cycleForm.addEventListener('submit', function(e) {
+  e.preventDefault()
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      startdate: newCycle.value
+    })
+  };
+
+  
 })
